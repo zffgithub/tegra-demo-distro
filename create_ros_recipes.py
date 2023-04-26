2a6f8ab4943d4ca894a800618112f238ee469c63
zffgithub <zffforgithub@gmail.com>
2023-04-26 23:02:36 +0800

    add run.py

---
+++ create_ros_recipes.py
@@ -0,0 +1,16 @@
+
+github_repos = []
+
+# https://docs.github.com/en/rest
+# https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-an-organization-repository
+def create_github_empty_repo(repo_name):
+    pass
+
+def create_github_repos():
+    pass
+
+def generate_ros_recipes():
+    pass
+
+if __name__ == "__main__":
+    pass
